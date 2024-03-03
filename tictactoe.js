7ac88408f57869c0c0a42da8fa3191d5e0031f42
sofoniasElala <126926375+sofoniasElala@users.noreply.github.com>
2024-03-02 20:06:37 -0500

    handle an edge case and refactor code

--- tictactoe.js
+++ tictactoe.js
@@ -6,28 +6,24 @@ const tictactoe = (function () {
         currentPlayer: this.playerOne,
         currentPlayerDisplay: document.querySelector('.current-player'),
         checkForWinner: function() {
+            let winner = false;
                 let winningCombinations = [[0,1,2], [3,4,5], [2,5,8], [6,7,8], [1,4,7], [0,3,6], [2,4,6], [0,4,8]]
                 if(this.currentPlayer.positions.length >= 3){
                     for(let nums of winningCombinations){
                         if(nums.every(num =>  this.currentPlayer.positions.includes(num))) {
                             showResults(`You Won ${this.currentPlayer.name}!`);
-                        } else if(this.playerOne.positions.length + this.playerTwo.positions.length == 9){
-                            showResults(`Draw!`);
-                        }
+                            winner = true;
+                            break;
+                        } 
                     }
+                }
+                if(this.playerOne.positions.length + this.playerTwo.positions.length == 9 && winner == false){
+                    showResults(`Draw!`);
                 }
              },
         changeCurrentPlayer: function() {
             this.currentPlayer = this.currentPlayer === this.playerOne ? this.playerTwo : this.playerOne; 
             display.setCurrentPlayer();
-        }
-    }
-
-
-    const gameBoard = {
-        board: [],
-        reset: function() {
-            this.board = [];
         }
     }
 
@@ -61,7 +57,6 @@ const tictactoe = (function () {
     }
 
     function placeMarker(pos){
-        gameBoard.board[pos] = game.currentPlayer.marker;
         game.currentPlayer.positions.push(pos);
         game.checkForWinner();
         game.changeCurrentPlayer();
@@ -87,7 +82,7 @@ const tictactoe = (function () {
 
     }
 
-    function showResults(status){
+    function showResults(status = 'draw'){
         const dialog = document.querySelector('.results');
         const statusDisplay = document.querySelector('.results>p');
         const restartButton = document.querySelector('.restart');
